0830a43dc7d81c06a8975c81c197a09659ffbeb2
convoymedia-paul <paul@convoymedia.com>
2020-03-17 17:48:38 +0000

    critical fix

--- page.php
+++ page.php
@@ -16,7 +16,7 @@ get_header(); ?>
 <div class="container">
 	<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 		<div class="content">
-			<?php the content(); ?>
+			<?php the_content(); ?>
 		</div>
 	<?php endwhile; ?>
 	<?php get_sidebar(); ?>
