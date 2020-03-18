55f3ef0b166bcc7b70de3b0e39d46ff30a4262ae
convoymedia-paul <paul@convoymedia.com>
2020-03-18 11:25:08 +0000

    viewport

--- header.php
+++ header.php
@@ -25,6 +25,7 @@
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap" rel="stylesheet"> 
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <?php
 	/* We add some JavaScript to pages with the comment form
 	 * to support sites with threaded comments (when in use).
